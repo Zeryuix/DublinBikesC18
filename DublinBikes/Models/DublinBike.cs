72275defa842be967107f3b1ec7d0acd96c6b1c8
Zeryuix <leo.mermet91@gmail.com>
2021-03-21 15:31:26 +0000

    model add

---
+++ DublinBikes/Models/DublinBike.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DublinBikes.Models
+{
+    public class DublinBike
+    {
+        public int Number { get; set; }
+        public string ContractName { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public float Latitude { get; set; }
+        public float Longitude { get; set; }
+        public bool Banking { get; set; }
+        public int Available_bikes { get; set; }
+        public int Available_stands { get; set; }
+        public int Capacity { get; set; }
+        public string Status { get; set; }
+    }
+}
